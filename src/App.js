4f29a644356ddf94f680bce1781a6b10f6eff1bf
xirade <6myusic6@gmail.com>
2022-01-23 17:06:37 +0200

    update project

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+
+const cardImages = [
+  { src: "/img/burger.png", matched: false },
+  { src: "/img/pancake.png", matched: false },
+  { src: "/img/pizza.png", matched: false },
+  { src: "/img/potato.png", matched: false },
+  { src: "/img/sandwich.png", matched: false },
+  { src: "/img/watermelon.png", matched: false },
+];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({
+        ...card,
+        id: Math.floor(Math.random() * (100000 - 1 + 1) + 1),
+      }));
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) =>
+          prevCards.map((card) => {
+            if (card.src === choiceOne.src) return { ...card, matched: true };
+            else return card;
+          })
+        );
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  // start a new game automatically
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Ximemory Food</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <p>Turns: {turns}</p>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <Card
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
